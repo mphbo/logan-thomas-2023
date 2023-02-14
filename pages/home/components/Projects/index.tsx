28905c2f8d90b334c0cb8648ee943f14c0debbda
Logan Thomas <logan.thomas@forsta.com>
2023-02-14 00:28:11 -0700

    added download resume button, no functionality yet

---
+++ pages/home/components/Projects/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles/Projects.module.scss";
+
+function Projects() {
+  return (
+    <div className={styles.projects}>
+      <p className={styles.title}>Projects</p>
+    </div>
+  );
+}
+
+export default Projects;
